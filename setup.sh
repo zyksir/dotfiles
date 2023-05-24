c1366d151cebaf12840dff962d025c9310019d58
zyksir <jessenykzhu@tencent.com>
2023-05-23 17:39:59 -0700

    comment vim plugin

---
+++ setup.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+git clone --depth=1 https://github.com/TamCore/autoupdate-oh-my-zsh-plugins ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/autoupdate
+git clone --depth=1 https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone --depth=1 https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone --depth=1 https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/plugins/zsh-completions
+git clone --depth=1 https://github.com/agkozak/zsh-z.git ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/plugins/zsh-z
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+
+cp zsh/zshrc ~/.zshrc
+cp vim/vimrc ~/.vimrc
+cp tmux/tmux.conf.ini .tmux.conf
+cp git/gitconfig.ini .gitconfig
+cp git/gitignore_global .gitignore_global
