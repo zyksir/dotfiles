40d129f9c93252ebe845f12d525de6031f905285
Yikai Zhu <yikai.zhu@baseten.com>
2025-08-09 10:25:24 +0800

    update aliases

--- aliases.sh
+++ aliases.sh
@@ -4,3 +4,11 @@ tnew() { tmux new-session -s "$1"; }
 tkill() { tmux kill-session -t "$1"; }
 tgo() { tmux a -t "$1"; }
 alias tls="tmux ls" 
+
+dexec() { docker exec -it "$1" /bin/bash; }
+
+alias k=kubectl
+alias kpg="kubectl get pods"
+kexec() { kubectl exec -it "$1" -- /bin/bash; }
+alias node1="k exec -it yikai-node1-dev-pod-h100-0 -- /bin/bash"
+alias node2="k exec -it yikai-dev-pod-h100-0 -- /bin/bash"
