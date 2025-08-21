69883f0640144d3fa710e7bc46f142669ee9d739
yikai Zhu <zhuyikai.zyk@gmail.com>
2025-08-21 19:48:20 +0800

    add alias

--- aliases.sh
+++ aliases.sh
@@ -5,10 +5,10 @@ tkill() { tmux kill-session -t "$1"; }
 tgo() { tmux a -t "$1"; }
 alias tls="tmux ls" 
 
-dexec() { docker exec -it "$1" /bin/bash; }
+dit() { docker exec -it "$1" /bin/bash; }
 
 alias k=kubectl
 alias kpg="kubectl get pods"
-kexec() { kubectl exec -it "$1" -- /bin/bash; }
+kit() { kubectl exec -it "$1" -- /bin/bash; }
 alias node1="k exec -it yikai-node1-dev-pod-h100-0 -- /bin/bash"
 alias node2="k exec -it yikai-dev-pod-h100-0 -- /bin/bash"
